ab0ae98edde0f4462a4a283c367b5470fd40a06e
Ali Zeaiter <ali.zeaiter@volvocars.com>
2023-03-17 13:24:02 +0100

    update license year

--- nextjs-site-navigation/next.config.js
+++ nextjs-site-navigation/next.config.js
@@ -1,4 +1,4 @@
-/*  Copyright 2022 Volvo Car Corporation
+/*  Copyright 2023 Volvo Car Corporation
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
